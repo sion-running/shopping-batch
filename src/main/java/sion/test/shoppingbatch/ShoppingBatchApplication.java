e59840aa72149e6b1d1de1a38da3458172ac4d4e
Sion <staythismoment.sion@gmail.com>
2024-01-16 23:35:22 +0900

    initial commit

---
+++ src/main/java/sion/test/shoppingbatch/ShoppingBatchApplication.java
@@ -0,0 +1,13 @@
+package sion.test.shoppingbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sion/test/shoppingbatch/ShoppingBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package sion.test.shoppingbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
