215d4759edb29b3521348087864d8c8111ed3457
Sion <staythismoment.sion@gmail.com>
2024-01-18 19:12:56 +0900

    스프링 배치 활성화를 위한 어노테이션 추가

--- src/main/java/sion/test/shoppingbatch/ShoppingBatchApplication.java
+++ src/main/java/sion/test/shoppingbatch/ShoppingBatchApplication.java
@@ -1,9 +1,11 @@
 package sion.test.shoppingbatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class ShoppingBatchApplication {
 
 	public static void main(String[] args) {
