38d96977e94e57adf7e537611149dc7bb01bee9f
Sion <staythismoment.sion@gmail.com>
2024-01-20 13:57:44 +0900

    simple job 생성 및 job launcher 클래스 추가

--- src/main/java/sion/test/shoppingbatch/JobInstanceConfiguration.java
+++ src/main/java/sion/test/shoppingbatch/JobInstanceConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class JobConfiguration {
+public class JobInstanceConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
