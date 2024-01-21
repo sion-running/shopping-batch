36bac7bb5b3c35820f77e44449fc1088e1f42c9c
Sion <staythismoment.sion@gmail.com>
2024-01-21 17:38:00 +0900

    ExecutionContext 동작/활용을 확인하기 위한 코드 작성

---
+++ src/main/java/sion/test/shoppingbatch/ExecutionContextTasklet1.java
@@ -0,0 +1,31 @@
+package sion.test.shoppingbatch;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExecutionContextTasklet1 implements Tasklet {
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        System.out.println("step1");
+
+        ExecutionContext jobExecutionContext = contribution.getStepExecution().getJobExecution().getExecutionContext();
+        ExecutionContext stepExecutionContext = contribution.getStepExecution().getExecutionContext();
+
+        String jobName = chunkContext.getStepContext().getStepExecution().getJobExecution().getJobInstance().getJobName();
+        String stepName = chunkContext.getStepContext().getStepExecution().getStepName();
+
+        if (jobExecutionContext.get("jobName") == null) { // 한 번도 job execution context 안에 jobName이라는 키로 값을 저장한 적이 없는 경우
+            jobExecutionContext.put("jobName", jobName);
+        }
+
+        if (stepExecutionContext.get("stepName") == null) {
+            stepExecutionContext.put("stepName", stepName);
+        }
+        return RepeatStatus.FINISHED;
+    }
+}
--- src/main/java/sion/test/shoppingbatch/JobExecutionConfiguration.java
+++ src/main/java/sion/test/shoppingbatch/JobExecutionConfiguration.java
@@ -3,12 +3,8 @@ package sion.test.shoppingbatch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,43 +14,46 @@ import org.springframework.context.annotation.Configuration;
 public class JobExecutionConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private final ExecutionContextTasklet1 executionContextTasklet1;
+    private final ExecutionContextTasklet2 executionContextTasklet2;
+    private final ExecutionContextTasklet3 executionContextTasklet3;
+    private final ExecutionContextTasklet4 executionContextTasklet4;
 
     @Bean
     public Job batchJob() {
-        return jobBuilderFactory.get("job")
+        return jobBuilderFactory.get("testJob")
                 .start(step1())
                 .next(step2())
+                .next(step3())
+                .next(step4())
                 .build();
     }
 
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("==================== ");
-                        System.out.println(">> hello spring batch ");
-                        System.out.println("==================== ");
-
-                        return RepeatStatus.FINISHED;
-                    }
-                }).build();
+                .tasklet(executionContextTasklet1)
+                .build();
     }
 
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                .tasklet(executionContextTasklet2)
+                .build();
+    }
 
-                        System.out.println("==================== ");
-                        System.out.println(">> step 2 was executed ");
-                        System.out.println("==================== ");
-                        throw new RuntimeException("step 2 failed");
-//                        return RepeatStatus.FINISHED;
-                    }
-                }).build();
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet(executionContextTasklet3)
+                .build();
+    }
+
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                .tasklet(executionContextTasklet4)
+                .build();
     }
 }
